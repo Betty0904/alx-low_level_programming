350bc66cab20470b26bf177bb95985f88aea19e3
root <tsegabetelhem0@gmail.com>
2023-10-09 08:23:23 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,11 +3,11 @@
  * main - print lowercase backwards
  * Return: 0
  */
-int main
+int main(void)
 {
 char x;
 for (x = 'z'; x > 'a'; x--)
 putchar(x);
-putchar('\n')
-return(0);
+putchar('\n');
+return (0);
 }

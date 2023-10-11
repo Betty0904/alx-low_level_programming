a5a1bdaffd842560e15770f980926755294e9546
root <tsegabetelhem0@gmail.com>
2023-10-11 07:38:14 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add(int, int) - check the code
+ *
+ * Return: sum result
+ */
+int add(inti, intj)
+{
+return (inti + intj);
+)
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * times_table - print the table 9 times starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+
